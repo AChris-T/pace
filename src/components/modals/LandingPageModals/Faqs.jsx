cf6977776a597eed4dd9b674b9eaf227ff51a983
chris <taiwoakinfenwa2019@gmail.com>
2024-05-28 18:53:51 +0100

    new

--- src/components/modals/LandingPageModals/Faqs.jsx
+++ src/components/modals/LandingPageModals/Faqs.jsx
@@ -9,7 +9,7 @@ const Faqs = () => {
   return (
     <div>
         <div className="bg-green-Primary_1 flex justify-center md:h-[150px] items-center">
-        <h3 className="font-normal  text-[48px] text-[#fff] flex flex-wrap text-center py-10">Frequently Asked Questions</h3>
+        <h3 className="font-normal text-[25px]  md:text-[40px] text-[#fff] flex flex-wrap text-center py-10">Frequently Asked Questions</h3>
         </div>
         <div className="flex flex-col items-center justify-center gap-3 mx-4 md:mx-0 my-11">
         <Accordion >
@@ -21,7 +21,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -31,7 +31,7 @@ const Faqs = () => {
         >
           <Typography className="text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>What is The Pace App ?</Typography>
         </AccordionSummary>
@@ -54,7 +54,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -64,7 +64,7 @@ const Faqs = () => {
         >
           <Typography className="text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>How do I earn on The Pace App?</Typography>
         </AccordionSummary>
@@ -87,7 +87,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -95,9 +95,9 @@ const Faqs = () => {
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography className="text-[24px] font-bold" style={{
+          <Typography className="md:text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>How to I withdraw my Earnings?</Typography>
         </AccordionSummary>
@@ -120,7 +120,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -130,7 +130,7 @@ const Faqs = () => {
         >
           <Typography className="text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>When can I withdraw my Earnings?</Typography>
         </AccordionSummary>
@@ -153,7 +153,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -163,7 +163,7 @@ const Faqs = () => {
         >
           <Typography className="text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>How much do The Pace App charge users?</Typography>
         </AccordionSummary>
@@ -186,7 +186,7 @@ const Faqs = () => {
           }}
           expandIcon={<ExpandMoreIcon style={{
             height:"75px",
-            width:"50px",
+            width:"40px",
             color:"white",
             }}
 
@@ -196,7 +196,7 @@ const Faqs = () => {
         >
           <Typography className="text-[24px] font-bold" style={{
             fontWeight:'bold',
-            fontSize:"24px",
+            fontSize:"20px",
             paddingLeft:'20px'
           }}>How Legit is The Pace App?</Typography>
         </AccordionSummary>
@@ -215,11 +215,11 @@ const Faqs = () => {
             <div className="w-[80%] bg-green-Primary_1 rounded-[30px] md:h-[255px] md:px-16 px-7 py-10 flex flex-col md:flex-row items-center justify-around ">
               <div className="flex flex-col gap-8 md:gap-0 ">                
                 <div className="flex flex-row items-center justify-start leading-normal text-center " >
-                <h3 className="text-[48px] font-normal text-[#fff] flex text-center">Start Earning today</h3>
+                <h3 className="md:text-[45px] text-[35px] font-normal text-[#fff] flex text-center">Start Earning today</h3>
                 <div className='hidden w-2 h-2 mt-20 ml-1 bg-white md:mt-3 md:flex '></div>
                 </div>
                 <div className="flex items-center justify-start -mt-5 ">
-                <h3 className="text-[48px] font-normal text-[#fff] leading-normal text-center">Download App for free</h3>
+                <h3 className="md:text-[45px] text-[35px] font-normal text-[#fff] leading-normal text-center">Download App for free</h3>
                 <div className='hidden w-2 h-2 mt-3 ml-1 bg-white md:flex'></div>
                 </div>
               </div>
